8d2e07cf692ac49d6d73e0dac31c69cf418540fd
Anna Wrang <annawrang@Annas-MacBook-Pro.local>
2020-09-01 16:57:46 +0200

    add gitlab config, add test

--- src/test/java/com/example/gitlabcitest/GitlabCiTestApplicationTests.java
+++ src/test/java/com/example/gitlabcitest/GitlabCiTestApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.gitlabcitest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GitlabCiTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void addition() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
 
 }

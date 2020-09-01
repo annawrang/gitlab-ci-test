c5f1b6ace7188a51ae5fc45a037d70ea23e1f215
Anna Wrang <annawrang@Annas-MacBook-Pro.local>
2020-09-01 14:38:43 +0200

    initial commit

---
+++ src/main/java/com/example/gitlabcitest/GitlabCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitlabcitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitlabcitest/GitlabCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitlabcitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabCiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
